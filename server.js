3ee8bb2720f877f4288ece91f6cf7297be274476
ofir h <houbara0@gmail.com>
2019-08-29 22:55:20 +0300

    chnage the app name

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/NORTHWINDEMPLOYEES'));
+app.use(express.static(__dirname + '/dist/NorthwindEmployees'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/NORTHWINDEMPLOYEES/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/NorthwindEmployees/index.html'));
 });
 
 // default Heroku PORT
